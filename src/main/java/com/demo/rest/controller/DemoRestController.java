45976e4970ec33294b5b82026f24672480c814d6
Simovanev <simovanev@abv.bg>
2024-08-06 05:19:47 +0300

    POJO to Jason via Jackson

--- src/main/java/com/demo/rest/controller/DemoRestController.java
+++ src/main/java/com/demo/rest/controller/DemoRestController.java
@@ -1,8 +1,12 @@
 package com.demo.rest.controller;
 
+import com.demo.rest.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
@@ -11,5 +15,13 @@ public class DemoRestController {
     @GetMapping("/hi")
     public String hello(){
         return "Hello World";
+    }
+    @GetMapping("/students")
+    public List<Student> theList(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student("Gosho","Peshev"));
+        students.add(new Student("Pesho","Geshev"));
+        students.add(new Student("Docho","Dochev"));
+        return students;
     }
 }
