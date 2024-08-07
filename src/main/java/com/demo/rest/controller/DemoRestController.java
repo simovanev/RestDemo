ae94e7fb203e5cbfc3ecca5e574df21950561413
Simovanev <simovanev@abv.bg>
2024-08-07 03:54:14 +0300

    @PostConstruct

--- src/main/java/com/demo/rest/controller/DemoRestController.java
+++ src/main/java/com/demo/rest/controller/DemoRestController.java
@@ -1,7 +1,9 @@
 package com.demo.rest.controller;
 
 import com.demo.rest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +14,23 @@ import java.util.List;
 @RequestMapping("/test")
 public class DemoRestController {
 
+    private final List<Student> students = new ArrayList<>();
+
+    @PostConstruct
+    public void populateStudents() {
+
+        students.add(new Student("Gosho","Peshev"));
+        students.add(new Student("Pesho","Geshev"));
+        students.add(new Student("Docho","Dochev"));
+    }
+
     @GetMapping("/hi")
     public String hello(){
         return "Hello World";
     }
-    @GetMapping("/students")
-    public List<Student> theList(){
-        List<Student> students = new ArrayList<Student>();
-        students.add(new Student("Gosho","Peshev"));
-        students.add(new Student("Pesho","Geshev"));
-        students.add(new Student("Docho","Dochev"));
-        return students;
+    @GetMapping("/students/{id}")
+    public Student theList(@PathVariable int id){
+
+        return students.get(id);
     }
 }
