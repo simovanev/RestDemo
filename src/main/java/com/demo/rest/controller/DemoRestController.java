71f01a5e34cfed00f0385c278c56ed167d71384e
Simovanev <simovanev@abv.bg>
2024-08-07 04:29:04 +0300

    Exception Handling

--- src/main/java/com/demo/rest/controller/DemoRestController.java
+++ src/main/java/com/demo/rest/controller/DemoRestController.java
@@ -2,10 +2,9 @@ package com.demo.rest.controller;
 
 import com.demo.rest.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +30,18 @@ public class DemoRestController {
     @GetMapping("/students/{id}")
     public Student theList(@PathVariable int id){
 
+        if (id>=students.size() || id < 0){
+            throw  new StudentNotFoundException("Student not found with id "+id);
+        }
         return students.get(id);
+    }
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex){
+        StudentErrorResponse error= new StudentErrorResponse();
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
     }
 }
